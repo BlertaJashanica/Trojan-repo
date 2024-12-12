273154153a20e664041e5b87505b235d595c936b
Blerta <91467900+BlertaJashanica@users.noreply.github.com>
2024-12-12 23:06:53 +0100

    Update system_enumeration.py

--- modules/system_enumeration.py
+++ modules/system_enumeration.py
@@ -24,3 +24,9 @@ class SystemEnumeration:
             # "running_processes": [p.info for p in psutil.process_iter(attrs=['pid', 'name'])]
         }
         return info
+
+security_tool = SystemEnumeration()
+# Voorbeeld van de System Enumeration module
+print("Gathering system information...")
+system_info = security_tool.system_enumeration_module.gather_info()
+print(json.dumps(system_info, indent=4))
